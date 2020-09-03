2f6be85fe9c216e84ca7ffd024c5de332d756313
setbap <ebr.sina@outlook.com>
2020-09-04 03:32:41 +0430

    clean file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
-import 'dart:math';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:todosin/person.dart';
+import 'package:todosin/sl.dart';
 
 main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (!kIsWeb) {
-    var dir = await getApplicationDocumentsDirectory();
-    await Hive.initFlutter(dir.path);
-  } else {
-    await Hive.initFlutter();
-  }
-  Hive.registerAdapter(PersonAdapter());
-  await Hive.openBox<Person>("persons");
+  setupLocator();
   runApp(MyApp());
 }
 
@@ -42,25 +28,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    var personsBox = Hive.box<Person>("persons");
-
     return Scaffold(
       appBar: AppBar(
         title: Text("sina"),
       ),
-      body: ListView.builder(
-        itemCount: personsBox.values.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(personsBox.getAt(index).name),
-          leading: Text(personsBox.getAt(index).age.toString()),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Text("add"),
-          onPressed: () {
-            personsBox.add(Person(age: Random().nextInt(100), name: "sina"));
-            setState(() {});
-          }),
+      body: Center(),
     );
   }
 }
---
+++ lib/sl.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+
+var locator = GetIt.I;
+
+void setupLocator() {
+  // locator.registerSingleton(DbManager());
+  // locator.registerFactory(() => ResualtClass(locator()));
+  print("serice locator");
+}
